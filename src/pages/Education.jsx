ad0774664bf74a41707a28ba5921e1536856d6c2
casper <casperChan@sampras.hk>
2024-12-05 18:37:31 +0800

    text updated

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,17 +1,66 @@
 import { AnimationWaveText } from "_components/AnimationWaveText";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Experience = () => {
+  const [w, setW] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setW(window.innerWidth); // Update state with the current window width
+    };
+
+    // Add the resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div className="Bg-content">
       <div className="Experience-list">
-        <div className="Title">
-          <AnimationWaveText text="Bachelor of Science (Honours) in Multimedia Technology and Innovation" />
-        </div>
+        {w > 600 ? (
+          <>
+            <div className="Title">
+              <AnimationWaveText text="Bachelor of Science (Honours) " />
+            </div>
+            <div className="Title">
+              <AnimationWaveText text="in Multimedia Technology and Innovation" />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="Title">
+              <AnimationWaveText text="Bachelor of Science" />
+            </div>
+            <div className="Title">
+              <AnimationWaveText text="(Honours) in Multimedia" />
+            </div>
+            <div className="Title">
+              <AnimationWaveText text="Technology and Innovation" />
+            </div>
+          </>
+        )}
 
-        <div className="Company">
-          <AnimationWaveText text="Technological and Higher Education Institute of Hong Kong" />
-        </div>
+        {w > 600 ? (
+          <div className="Company">
+            <AnimationWaveText text="Technological and Higher Education Institute of Hong Kong" />
+          </div>
+        ) : (
+          <>
+            <div className="Company">
+              <AnimationWaveText text="Technological and" />
+            </div>
+            <div className="Company">
+              <AnimationWaveText text="Higher Education Institute " />
+            </div>
+            <div className="Company">
+              <AnimationWaveText text="of Hong Kong" />
+            </div>
+          </>
+        )}
+
         <div className="Container">
           <div className="Detail">
             {" "}
@@ -23,13 +72,36 @@ const Experience = () => {
         </div>
       </div>
       <div className="Experience-list">
-        <div className="Title">
-          <AnimationWaveText text="Higher Diploma in Game Software Development" />
-        </div>
+        {w > 600 ? (
+          <div className="Title">
+            <AnimationWaveText text="Higher Diploma in Game Software Development" />
+          </div>
+        ) : (
+          <>
+            <div className="Title">
+              <AnimationWaveText text="Higher Diploma in" />
+            </div>{" "}
+            <div className="Title">
+              <AnimationWaveText text="Game Software Development" />
+            </div>
+          </>
+        )}
 
-        <div className="Company">
-          <AnimationWaveText text="Hong Kong Institute of Vocational Education, TY" />
-        </div>
+        {w > 600 ? (
+          <div className="Company">
+            <AnimationWaveText text="Hong Kong Institute of Vocational Education, TY" />
+          </div>
+        ) : (
+          <>
+            <div className="Company">
+              <AnimationWaveText text="Hong Kong" />
+            </div>
+            <div className="Company">
+              <AnimationWaveText text="Institute of Vocational Education, TY" />
+            </div>
+          </>
+        )}
+
         <div className="Container">
           <div className="Detail">
             {" "}
